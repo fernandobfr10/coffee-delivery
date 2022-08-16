250c1b99adf3b50e11a084dd59dc90b3528c7272
Fernando Junior <fernando.junior85@outlook.com>
2022-08-16 16:10:27 -0300

    styles: Create GlobalStyles && Default Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+
+import { defaultTheme } from './styles/theme/default'
+
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
